780962f47791503d156d28e8435587db3f681c1b
nghia <nghiangungoc>
2022-03-17 20:15:46 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./configs/viewEngine";
+import webRoutes from "./routes/web";
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//config view Engine
+viewEngine(app);
+
+//config web routes
+webRoutes(app);
+
+
+
+let port = process.env.PORT || 8083;
+
+app.listen(port, () => {
+    console.log("App is running at the port " + port);
+})
---
+++ src/services/homservies.js
@@ -0,0 +1,64 @@
+import request from "request";
+require('dotenv').config();
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+let callSendAPI = (sender_psid, response) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v13.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+
+}
+let getUserName = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            "method": "GET",
+        }, (err, res, body) => {
+            console.log(body);
+            if (!err) {
+                body = JSON.parse(body);
+                // "first_name":"peter",
+                // "last_name":"Chang",
+                let username = `${body.last_name} ${body.first_name}`
+                resolve(username);
+            } else {
+                console.error("Unable to send message:" + err);
+                reject(err);
+            }
+        });
+    })
+}
+let handleGetStarted = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let username = await getUserName(sender_psid);
+            let response = { "text": `Ok. xin chào mừng bạn ${username} đến với nhà hàng của nghĩa` };
+            await callSendAPI(sender_psid, response);
+            resolve('done');
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+module.exports = {
+    handleGetStarted: handleGetStarted,
+}
